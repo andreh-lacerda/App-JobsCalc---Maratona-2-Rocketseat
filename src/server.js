7581007698136f656142bdc85aae6a51471bf6d1
André Lacerda <andreh.lacerda@hotmail.com>
2021-05-13 14:25:37 -0300

    add. models, controllers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes")
+const path = require("path")
 
 //usando template engine
 server.set("view engine", "ejs")
+
+server.set("views", path.join(__dirname, "views"))
 
 //habilitar arquivos statics
 server.use(express.static("public"))
@@ -13,4 +16,5 @@ server.use(express.urlencoded({ extended: true }))
 
 //routes
 server.use(routes)
+
 server.listen(3000, () => console.log("rodando"))
